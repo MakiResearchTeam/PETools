503bd5bfb50cfdb1959e4abce5531500bfacea94
TaplierShiru <45129308+TaplierShiru@users.noreply.github.com>
2021-01-18 17:51:58 +0400

    fix imports

--- petools/tools/utils/nns_tools/keypoint_tools.py
+++ petools/tools/utils/nns_tools/keypoint_tools.py
@@ -1,19 +1,4 @@
-# Copyright (C) 2020  Igor Kilbas, Danil Gribanov
-#
-# This file is part of MakiPoseNet.
-#
-# MakiPoseNet is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# MakiPoseNet is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
 
 def scale_predicted_kp(predictions: list, model_size: tuple, source_size: tuple):
     # predictions shape - (N, num_detected_people)
