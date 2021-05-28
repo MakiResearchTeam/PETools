e45c0700f9f93308ba57078a0c8573ff79ac9a64
oKatanaaa <whitemarsstudios@gmail.com>
2021-05-28 17:24:52 +0400

    refactor(GPUModel): now it is inherited from ProtobufModel

--- petools/gpu/gpu_model.py
+++ petools/gpu/gpu_model.py
@@ -1,34 +1,28 @@
 import tensorflow as tf
 
-from ..core import Model
+from ..core import Model, ProtobufModel
 from petools.tools.utils.tf_tools import load_graph_def
 
 
-class GpuModel(Model):
+class GpuModel(ProtobufModel, Model):
     def __init__(
             self,
             pb_path, input_name,
             paf_name, ind_name, peaks_score_name, upsample_size_name='upsample_size',
             session=None
     ):
-        self.__graph_def = load_graph_def(pb_path)
         self.__in_x = tf.placeholder(dtype=tf.float32, shape=[1, None, None, 3], name='in_x')
         self.__upsample_size = tf.placeholder(dtype=tf.int32, shape=(2), name='upsample')
-        self.__resized_paf, self.__indices, self.__peaks_score = tf.import_graph_def(
-            self.__graph_def,
+
+        super(GpuModel, self).__init__(
+            protobuf_path=pb_path,
             input_map={
                 input_name: self.__in_x,
                 upsample_size_name: self.__upsample_size
             },
-            return_elements=[
-                paf_name,
-                ind_name,
-                peaks_score_name
-            ]
+            output_tensors=[paf_name, ind_name, peaks_score_name],
+            session=session
         )
-        if session is None:
-            session = tf.Session()
-        self.__sess = session
 
     def predict(self, norm_img):
         """
@@ -46,13 +40,8 @@ class GpuModel(Model):
             Single element of this list is a List of classes Human which were detected.
         """
         resize_to = norm_img[0].shape[:2]
-
-        batched_paf, indices, peaks = self.__sess.run(
-            [self.__resized_paf, self.__indices, self.__peaks_score],
-            feed_dict={
+        batched_paf, indices, peaks = super(GpuModel, self).predict(feed_dict={
                 self.__in_x: norm_img,
                 self.__upsample_size: resize_to
-            }
-        )
-
+        })
         return batched_paf, indices, peaks
