90ca409a2b378e011c7e313bcd6e598358a2b466
TaplierShiru <cool.danik01@yandex.ru>
2021-06-11 12:18:07 +0400

    update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,13 @@ import setuptools
 setup(
     name='PETools',
     packages=setuptools.find_packages(),
-    package_data={'petools': ['tools/estimate_tools/pafprocess/*.so', 'gpu/3d_converter_stats/*', 'cpu/3d_converter_stats/*']},
+    package_data={'petools': [
+            'tools/estimate_tools/pafprocess/*.so',   # Binary files for linux
+            'tools/estimate_tools/pafprocess/*.pyd',  # Binary files for win
+            'gpu/3d_converter_stats/*',
+            'cpu/3d_converter_stats/*'
+        ]
+    },
     version='1.2.3',
     description='A set of tools to use pose estimation models',
     long_description='...',
