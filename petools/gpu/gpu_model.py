cbb234c4d539e154cf9d2b498c3968a04a5f351e
oKatanaaa <whitemarsstudios@gmail.com>
2021-06-23 13:37:22 +0400

    fix(GpuModel): __init__ args

--- petools/gpu/gpu_model.py
+++ petools/gpu/gpu_model.py
@@ -5,14 +5,12 @@ class GpuModel(ProtobufModel, Model):
     def __init__(
             self,
             pb_path, input_name,
-            paf_name, ind_name, peaks_score_name, upsample_size_name='upsample_size:0',
-            session=None
+            paf_name, ind_name, peaks_score_name, upsample_size_name='upsample_size:0'
     ):
         super(GpuModel, self).__init__(
             protobuf_path=pb_path,
-            input_map=[input_name, upsample_size_name],
-            output_tensors=[paf_name, ind_name, peaks_score_name],
-            session=session
+            input_tensor_names=[input_name, upsample_size_name],
+            output_tensors_names=[paf_name, ind_name, peaks_score_name]
         )
 
     def predict(self, norm_img):
