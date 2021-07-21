74e0ec9c3225c6009ecb33a6e310468faffee953
TaplierShiru <cool.danik01@yandex.ru>
2021-07-22 00:13:57 +0400

    fix(keypoint_tools): return value

--- petools/tools/utils/nns_tools/keypoint_tools.py
+++ petools/tools/utils/nns_tools/keypoint_tools.py
@@ -1,7 +1,7 @@
 from numba import njit
 
 @njit
-def scale_predicted_kp(predictions: list, model_size: tuple, source_size: tuple):
+def scale_predicted_kp(predictions, model_size, source_size):
     # predictions shape - (N, num_detected_people)
     # scale predictions
     scale = [source_size[1] / model_size[1], source_size[0] / model_size[0]]
@@ -11,5 +11,5 @@ def scale_predicted_kp(predictions: list, model_size: tuple, source_size: tuple)
         single_human_np[:, 0] *= scale[0]
         single_human_np[:, 1] *= scale[1]
 
-    return single_image_pr
+    return predictions
 
