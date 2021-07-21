002922def441fa8aa1eba74096d9c6ba57f56c43
TaplierShiru <cool.danik01@yandex.ru>
2021-07-22 00:12:44 +0400

    fix(keypoint_tools): name input parameter

--- petools/tools/utils/nns_tools/keypoint_tools.py
+++ petools/tools/utils/nns_tools/keypoint_tools.py
@@ -1,12 +1,12 @@
 from numba import njit
 
 @njit
-def scale_predicted_kp(single_image_pr: list, model_size: tuple, source_size: tuple):
+def scale_predicted_kp(predictions: list, model_size: tuple, source_size: tuple):
     # predictions shape - (N, num_detected_people)
     # scale predictions
     scale = [source_size[1] / model_size[1], source_size[0] / model_size[0]]
-    for h_indx in range(len(single_image_pr)):
-        single_human_np = single_image_pr[h_indx]
+    for h_indx in range(len(predictions)):
+        single_human_np = predictions[h_indx]
         # multiply
         single_human_np[:, 0] *= scale[0]
         single_human_np[:, 1] *= scale[1]
