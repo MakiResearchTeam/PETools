6d6224b2ee464c4447c49b2cc75aa956d1223513
TaplierShiru <cool.danik01@yandex.ru>
2021-07-22 11:16:50 +0400

    add(preprocess): try wo numba, remove creation of numpy arr

--- petools/tools/utils/nns_tools/preprocess.py
+++ petools/tools/utils/nns_tools/preprocess.py
@@ -230,7 +230,7 @@ def caffe_numba(x: np.ndarray, dtype=np.float32) -> np.ndarray:
     x = x.astype(dtype)
     # Vgg like normalization
     #         B        G        R
-    mean = np.array([103.939, 116.779, 123.68], dtype=dtype)
+    mean = [103.939, 116.779, 123.68]
 
     # Zero-center by mean pixel
     x -= mean
