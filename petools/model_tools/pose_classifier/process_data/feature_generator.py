fix(feature_generator): new shape, fix calculation to new

petools/model_tools/pose_classifier/process_data/feature_generator.py
@@ -76,12 +76,11 @@ class FeatureGenerator:
         # Hip dist to hip-neck
         if np.prod(points[11]) < 1e-3 or np.prod(points[10]) < 1e-3 or \
             np.prod(points[22]) < 1e-3 or np.prod(points[2]) < 1e-3 or np.prod(points[22] - points[2]) < 1e-3:
-            dist = np.zeros((2,), dtype=np.float32)
+            dist = np.float32(0.0)
         else:
             ev_dist = lambda x: np.sqrt(np.sum(np.square(x)))
             dist = ev_dist(points[11] - points[10]) / ev_dist(points[22] - points[2])
-        features[-1] = dist[0] # x
-        features[-2] = dist[1] # y
+        features[-1] = dist
 
         dist = np.sign(points[2] - points[22])
-        features[-3] = dist[1] # y
+        features[-2] = dist[1] # y