9b72ad4bc98a35aed1265e6c5b1723766e1a1f39
TaplierShiru <cool.danik01@yandex.ru>
2021-09-01 23:12:21 +0400

    udpate version

--- petools/__version__.py
+++ petools/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'petools'
 __license__ = 'To be chosen...'
-__version__ = '1.3.7'
+__version__ = '1.4.0'
 __author__ = 'Kilbas Igor, Gribanov Danil'
 __contact__ = 'cool.danik01@yandex.ru'
 __url__ = 'https://github.com/MakiResearchTeam/PETools.git'
