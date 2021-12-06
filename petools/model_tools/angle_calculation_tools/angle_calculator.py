f17388cc8a390c8b78466955f5c0169a860530c0
oKatanaaa <whitemarsstudios@gmail.com>
2021-12-06 16:26:26 +0400

    new: implemented angle_calculator

--- petools/model_tools/angle_calculation_tools/angle_calculator.py
+++ petools/model_tools/angle_calculation_tools/angle_calculator.py
@@ -1,12 +1,25 @@
 import copy
 from typing import List, Mapping, Tuple, Dict
-
 import numpy as np
+import math
 
 from petools.core import PosePredictorInterface
+from .angle_math import *
 
 
 class AngleCalculator:
+    right_shoulder_angle = 'right_shoulder_angle'
+    left_shoulder_angle = 'left_shoulder_angle'
+    right_hip_angle = 'right_hip_angle'
+    left_hip_angle = 'left_hip_angle'
+    right_shoulder_normal_angle = 'right_shoulder_normal_angle'
+    left_shoulder_normal_angle = 'left_shoulder_normal_angle'
+    right_hip_normal_angle = 'right_hip_normal_angle'
+    left_hip_normal_angle = 'left_hip_normal_angle'
+    right_elbow_angle = 'right_elbow_angle'
+    left_elbow_angle = 'left_elbow_angle'
+    right_knee_angle = 'right_knee_angle'
+    left_knee_angle = 'left_knee_angle'
 
     def __init__(self, name_angle_to_calc: List[str]):
         """
@@ -19,7 +32,18 @@ class AngleCalculator:
         """
         self._name_angle_to_calc = name_angle_to_calc
         self._name_angle_2_func = {
-            "mmmm_angle": AngleCalculator.calculate_angle_1,
+            AngleCalculator.right_shoulder_angle: right_shoulder_angle,
+            AngleCalculator.left_shoulder_angle: left_shoulder_angle,
+            AngleCalculator.right_hip_angle: right_hip_angle,
+            AngleCalculator.left_hip_angle: left_hip_angle,
+            AngleCalculator.right_shoulder_normal_angle: right_shoulder_normal_angle,
+            AngleCalculator.left_shoulder_normal_angle: left_shoulder_normal_angle,
+            AngleCalculator.right_hip_normal_angle: right_hip_normal_angle,
+            AngleCalculator.left_hip_normal_angle: left_hip_normal_angle,
+            AngleCalculator.right_elbow_angle: right_elbow_angle,
+            AngleCalculator.left_elbow_angle: left_elbow_angle,
+            AngleCalculator.right_knee_angle: right_knee_angle,
+            AngleCalculator.left_knee_angle: left_knee_angle
         }
 
     def __call__(
@@ -44,8 +68,8 @@ class AngleCalculator:
             dict_3d - dict of 3d prediction {"pn": [val_x, val_y, val_z, val_prob]}
             classification_info - tuple from classificator ("name_pose": prob_value)
         lengths : dict
-            Dict with data {human_id: {"name_proportion": length_proportion} }
-            Where `length_proportion` - float value
+            Contains lengths of limbs for each human in `preds` dictionary:
+            {human_id: {"limb_name": length} }, `length` is float.
 
         Return
         ------
@@ -55,35 +79,32 @@ class AngleCalculator:
         """
         new_dict = copy.deepcopy(preds)
 
-        for i, pred, pred_new in enumerate(zip(preds[PosePredictorInterface.HUMANS], new_dict)):
+        for i, (pred, pred_new) in enumerate(
+                zip(preds[PosePredictorInterface.HUMANS], new_dict[PosePredictorInterface.HUMANS])
+        ):
             human_id = str(pred[0])
-            data_2d = np.asarray(list(pred[1].values()), dtype=np.float32)[:, :-1] # Skip prob values
-            data_3d = np.asarray(list(pred[2].values()), dtype=np.float32)[:, :-1] # Skip prob values
-            name_pose = str(pred[-1][0])
+            points2d = pred[1]
+            points3d = pred[2]
+            pose_name = str(pred[-1][0])
 
             # Take length for this human
-            length_human = lengths.get(human_id)
-            if length_human is None:
-                continue # Skip it???? Dont know
+            limb_lengths = lengths.get(human_id)
+            if limb_lengths is None:
+                print(f'Found no limb length info for human with id={human_id}')
+                continue
 
             result_angle_dict = {}
             for name_angle, func in self._name_angle_2_func:
                 result_angle_dict[name_angle] = func(
-                    data_2d=data_2d, data_3d=data_3d,
-                    name_pose=name_pose, lengths=length_human
+                    data_2d=points2d, data_3d=points3d,
+                    name_pose=pose_name, lengths=limb_lengths
                 )
 
             new_tuple = (
-                pred_new[0], pred_new[1], pred_new[2], pred_new[3], # Keep original dict on safe!
+                *pred_new,  # Keep original dict safe!
                 result_angle_dict
             )
             new_dict[PosePredictorInterface.HUMANS][i] = new_tuple
 
         return new_dict
-
-    @staticmethod
-    def calculate_angle_1(
-            data_2d: np.ndarray, data_3d: np.ndarray,
-            name_pose: str, lengths: Mapping[str, float], **kwargs) -> float:
-        pass
 
